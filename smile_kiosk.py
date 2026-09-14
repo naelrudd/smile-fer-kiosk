44eda559849cd1612f38b72a777cada1df5c6e29
Nael Rudy <natanaelrudyhadinata@gmail.com>
2026-09-14 12:17:51 +0700

    Fine-tune emoji vertical alignment in pill

--- smile_kiosk.py
+++ smile_kiosk.py
@@ -153,10 +153,10 @@ def visualize(frame, faces, history):
         text_y = py1 + pill_h - 14
         cv.putText(output, label_text, (px1 + 60, text_y), FONT, 0.75, (255, 255, 255), 2, cv.LINE_AA)
 
-        # Draw emoji on the left of pill, vertically centered
+        # Draw emoji on the left of pill, vertically centered inside pill
         emoji_size = 28
         emoji_x = px1 + 12
-        emoji_y = py1 + (pill_h - emoji_size) // 2 + 1
+        emoji_y = py1 + (pill_h - emoji_size) // 2 - 8
         output = draw_emoji_bgr(output, info['emoji'], (emoji_x, emoji_y), size=emoji_size)
 
     return output, history
